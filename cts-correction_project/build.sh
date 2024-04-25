288dce282c696844b694c602dc61928219bf8aeb
Hicham El kettani <s297413@studenti.polito.it>
2024-04-25 22:36:45 +0200

    graph-data

--- cts-correction_project/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker build --rm -f Dockerfile --build-arg TZ="Europe/Rome" --network host -t inrim_cts_correction:latest .
-
-docker compose -f docker-compose-mock.yml stop
-docker compose -f docker-compose-mock.yml build --no-cache .
-docker compose -f docker-compose-mock.yml up
-
